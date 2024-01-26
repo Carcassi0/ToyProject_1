e72f04a89e476bbed0f5188b60ae244457ec287f
WEIUIJONG <dnldmlwhd@ajou.ac.kr>
2024-01-26 21:53:09 +0900

    Initial Commit

---
+++ flutter/flutter.h
@@ -0,0 +1,18 @@
+//
+//  flutter.h
+//  flutter
+//
+//  Created by 위의종 on 1/26/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for flutter.
+FOUNDATION_EXPORT double flutterVersionNumber;
+
+//! Project version string for flutter.
+FOUNDATION_EXPORT const unsigned char flutterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <flutter/PublicHeader.h>
+
+
